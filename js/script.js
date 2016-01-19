a3f00d27a0dcf53edd45bfdade1c5fe3f70c1531
Carl Humphreys <carlh.crimson@gmail.com>
2016-01-19 10:50:48 +0000

    Filling glass done

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 	Metaballs with help from:
 	http://www.somethinghitme.com/2012/06/06/2d-metaballs-with-canvas/
 */
-
+var animating = true;
 
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
@@ -27,12 +27,14 @@ maskImage.src = "../images/beer.jpg";
 var threshold = 200;
 var gravity = 9;
 
+var radiansPerSec = Math.PI * 2/(80*60);
+var radians = 0;
 
 var xCiderVel = 0.5;
-var yCiderVel = 2;
+var yCiderVel = 0;
 
 var xCider = 0;
-var yCider = 0;
+var yCider = 10;
 // shim layer with setTimeout fallback - Paul Irish
 window.requestAnimFrame = (function(){
   return  window.requestAnimationFrame       ||
@@ -63,7 +65,7 @@ var createParticles = function(n) {
 			yPos = canvas.height,
 			xVel = Math.random() * 5 - 2.5,
 			yVel = Math.random() * -2,			
-			rad = 100;
+			rad = 50;
 
 		// Create a particle with the above values and push to an array	
 		var p = new Particle(xPos, yPos, xVel, yVel, rad);
@@ -72,58 +74,71 @@ var createParticles = function(n) {
 };
 
 var render = function() {
-	window.requestAnimFrame(render);
-	// Clear temp canvas
-	tempCtx.clearRect(0,0,width,height);
+	if (animating) {
+		window.requestAnimFrame(render);
+		// Clear temp canvas
+		tempCtx.clearRect(0,0,width,height);
 
-	// xCider += xCiderVel;
-	yCider += yCiderVel;
+		// Movement of cider image to give more fluid effect
+		xCider += xCiderVel;
+		yCider += yCiderVel;
 
-	for (var i = 0; i < particlesArray.length; i++) {
-		var particle = particlesArray[i];
+		for (var i = 0; i < particlesArray.length; i++) {
+			var particle = particlesArray[i];
 
-		particle.velocity.y += gravity/60;
+			// Gravity applied to particles (~60 fps)
+			particle.velocity.y += gravity/60;
 
-		// Check if particles are going off-screen, if so then put on other side of canvas to give continuous movement
-		if (particle.position.x > width) {
-			particle.velocity.x *= -0.8;
-		}
-		if (particle.position.x < 0) {
-			particle.velocity.x *= -0.8;
-		}
-		if (particle.position.y > height) {
-			particle.velocity.y *= -0.8;
-		}
-		if (particle.position.y < 0) {
-			particle.velocity.y *= -0.8;
-		}
-		
-		// Moving cider image		
-		if (yCider >= 50 || yCider < -50) {
+			// Check if particles are going off-screen, if so then put on other side of canvas to give continuous movement
+			if (particle.position.x > width) {
+				particle.velocity.x *= -0.8;
+			}
+			if (particle.position.x < 0) {
+				particle.velocity.x *= -0.8;
+			}
+			if (particle.position.y > height) {
+				particle.velocity.y *= -0.8;
+			}
+			if (particle.position.y < 0) {
+				particle.velocity.y *= -0.8;
+			}
+			// "Fill up" glass by increasing the size of the particles
+			if (particle.radius < 650) {
+				particle.radius += (80/60);
+			}
+			// Once filled stop animating
+			else {
+				animating = false;
+			}
 			
-			yCiderVel -= 1/60;
+			// Moving cider image		
+			xCiderVel = (1/10) * Math.sin(2*radians);
+			yCiderVel = (1/10)* Math.sin(0.5*radians);
+
+			radians += radiansPerSec;
+
+			// Update positions based on velocity
+			particle.position.x += particle.velocity.x;
+			particle.position.y += particle.velocity.y;
+		
+			tempCtx.globalCompositeOperation = "source-over";
+			// Update colour positions on temp canvas
+			tempCtx.beginPath();
+			var gradient = tempCtx.createRadialGradient(particle.position.x, particle.position.y, 0, particle.position.x, particle.position.y, particle.radius);
+			gradient.addColorStop(0, 'rgba(' + particle.colour.red + ',' + particle.colour.green + ', ' + particle.colour.blue + ',1)');
+			gradient.addColorStop(1, 'rgba(' + particle.colour.red + ',' + particle.colour.green + ', ' + particle.colour.blue + ',0)');
+			tempCtx.fillStyle = gradient;
+			tempCtx.arc(particle.position.x, particle.position.y, particle.radius, 0, 2* Math.PI);
+			tempCtx.fill();
+
+			//Draw cider
+			tempCtx.globalCompositeOperation = "source-in";
+			tempCtx.drawImage(maskImage,(xCider - 2.5),yCider, canvas.width, canvas.height);
+
 		}
-		// Update positions based on velocity
-		particle.position.x += particle.velocity.x;
-		particle.position.y += particle.velocity.y;
-	
-		tempCtx.globalCompositeOperation = "source-over";
-		// Update colour positions on temp canvas
-		tempCtx.beginPath();
-		var gradient = tempCtx.createRadialGradient(particle.position.x, particle.position.y, 0, particle.position.x, particle.position.y, particle.radius);
-		gradient.addColorStop(0, 'rgba(' + particle.colour.red + ',' + particle.colour.green + ', ' + particle.colour.blue + ',1)');
-		gradient.addColorStop(1, 'rgba(' + particle.colour.red + ',' + particle.colour.green + ', ' + particle.colour.blue + ',0)');
-		tempCtx.fillStyle = gradient;
-		tempCtx.arc(particle.position.x, particle.position.y, particle.radius, 0, 2* Math.PI);
-		tempCtx.fill();
-
-		//Draw cider
-		tempCtx.globalCompositeOperation = "source-in";
-		tempCtx.drawImage(maskImage,xCider,yCider, canvas.width, canvas.height);
-
+		// Send to function that takes pixel data and implements alpha cut-off - then draws to the main canvas
+		metaballDraw();
 	}
-	// Send to function that takes pixel data and implements alpha cut-off - then draws to the main canvas
-	metaballDraw();
 
 };
 
@@ -145,7 +160,7 @@ var metaballDraw = function() {
 };
 
 var init = function() {
-	createParticles(50);
+	createParticles(10);
 	maskImage.onload = function() {
 		render();
 	};
